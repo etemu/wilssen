682410ba0aefb965b8b4440608e684ceade0477a
Leon Rische <leon.rische@me.com>
2012-12-05 19:22:01 +0100

    added comments

--- software/RGBlink/examples/TrafficLight/TrafficLight.ino
+++ software/RGBlink/examples/TrafficLight/TrafficLight.ino
@@ -1,16 +1,19 @@
 #include <RGBlink.h>
 
-LED led1(11,9,10);
+//define your RGB-LED like this:
+// LED led(red_pin,green_pin,blue_pin);
+LED led(11,9,10);
 
 void setup()   {                
 }
 
 void loop()                     
 {
-  led1.setColor(red);
+  //there are various presaved colors like red, green, yellow, blue and white
+  led.setColor(red);
   delay(1000);
-  led1.setColor(yellow);
+  led.setColor(yellow);
   delay(1000);
-  led1.setColor(green);
+  led.setColor(green);
   delay(1000);
 }
