17da3b3ff08ac9fb34d71046f7af7962f6c6998f
Alex Shure <shure@etemu.com>
2013-11-05 20:39:23 +0100

    m cleanup

--- software/lednode/lednode.ino
+++ software/lednode/lednode.ino
@@ -9,23 +9,17 @@
 #define USE_EEPROM // read nodeID and network settings from EEPROM at bootup, overwrites nodeID and MAC.
 #define LEDPIN 6
 #define KEEPALIVE 0 // keep connections alive with regular polling to node 0
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(8, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(8, LEDPIN, NEO_GRB + NEO_KHZ800); // number of pixels in strip, pin number, pixel type flags
 
 RF24 radio(A0,10); // CE, CS. CE at pin A0, CSN at pin 10
-RF24Network network(radio);
+RF24Network network(radio); // mesh network layer 
 
-
-const unsigned long interval = 2000;
+const unsigned long interval = 2000; // KEEPALIVE interval in [ms]
 byte sweep=0;
 byte nodeID = 1; // Unique Node Identifier (2...254) - also the last byte of the IPv4 adress, not used if USE_EEPROM is set
-uint16_t this_node = 00; // always begin with 0 for octal declaration
+uint16_t this_node = 00; // always begin with 0 for octal declaration, not used if USE_EEPROM is set
+// Debug variables, TODO: don't initialize if DEBUG is set to 0
 unsigned long iterations=0;
 unsigned long errors=0;
 unsigned int loss=0;
@@ -36,12 +30,12 @@ static unsigned long microRollovers=0; // variable that permanently holds the nu
 static unsigned long halfwayMicros = 2147483647; // this is halfway to the max unsigned long value of 4294967296
 static boolean readyToRoll = false; // tracks whether we've made it halfway to rollover
 
-const short max_active_nodes = 32;
+const short max_active_nodes = 32; // the size of the array with active nodes
 uint16_t active_nodes[max_active_nodes];
 short num_active_nodes = 0;
 short next_ping_node_index = 0;
 unsigned long last_time_sent;
-unsigned long updates = 0;
+unsigned int updates = 0; // has to be changed to unsigned long if the interval is too long
 void add_node(uint16_t node);
 boolean send_T(uint16_t to);
 void send_L1(int to, int _b);
@@ -52,7 +46,7 @@ void ledupdate(byte* ledmap);
 void p(char *fmt, ... );
 
 void ledst(int sta=127){
-  uint32_t c;
+  uint32_t c=0;
   switch (sta) {
   case 0:
     c=leds.Color(0, 0, 0);
@@ -94,7 +88,9 @@ void ledst(int sta=127){
   leds.setPixelColor(0, c);
   leds.show();
 }
-bool strobe=1;
+
+bool strobe=1; 
+
 void setup(void)
 {
   pinMode(A6, INPUT); // some nodes have a sense wire to the 3v3 for the NRF24 module via external LDO
@@ -110,7 +106,7 @@ void setup(void)
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-    #ifdef USE_EEPROM
+#ifdef USE_EEPROM
   nodeID=EEPROM.read(0);
   Serial.print(F("EEPROM, "));
 #endif
@@ -121,7 +117,7 @@ void setup(void)
   Serial.print(F("EEPROM, "));
 #endif
   Serial.print(this_node,OCT);
-  Serial.print(", (dec): ");
+  Serial.print(F(", (dec): "));
   Serial.print(this_node,DEC);
   Serial.println();
   SPI.begin(); // SPI for the NRF24
@@ -129,7 +125,7 @@ void setup(void)
   // The amplifier gain can be set to RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_MED=-6dBM, and RF24_PA_MAX=0dBm.
   radio.setPALevel(RF24_PA_MAX); // transmitter gain value (see above)
   network.begin( 1, this_node ); // fixed radio channel, node ID
-  Serial.print(F("UID: "));
+  Serial.print(F("UID: \n"));
   p("%010ld: Starting up\n", millis());
   colorWipe(leds.Color(100, 0, 0), 100); // Red
   colorWipe(leds.Color(0, 100, 0), 100); // Green
@@ -143,39 +139,42 @@ void setup(void)
     100,50,50,
     100,50,50,
     100,50,50,
-    0,150,0  };
+    0,150,0    };
   ledupdate(ledmap);
   colorWipe(leds.Color(0, 0, 0), 0); // clear
   ledst(1);
   radio.printDetails(); // print NRF config registers. Does not work with NRF24network right now?!
-  
+
 }
 PROGMEM prog_uchar red[]=
-{255,0,0,
-255,0,0,
-255,0,0,
-255,0,0,
-255,0,0,
-255,0,0,
-255,0,0,
-255,0,0};
+{
+  255,0,0,
+  255,0,0,
+  255,0,0,
+  255,0,0,
+  255,0,0,
+  255,0,0,
+  255,0,0,
+  255,0,0};
 PROGMEM prog_uchar blue[]=
-{0,0,255,
-0,0,255,
-0,0,255,
-0,0,255,
-0,0,255,
-0,0,255,
-0,0,255,
-0,0,255};
-byte pat1[]={10,0,0,
-10,10,0,
-0,10,0,
-0,10,10,
-0,0,10,
-10,0,10,
-10,5,0,
-5,16,5};
+{
+  0,0,255,
+  0,0,255,
+  0,0,255,
+  0,0,255,
+  0,0,255,
+  0,0,255,
+  0,0,255,
+  0,0,255};
+byte pat1[]={
+  10,0,0,
+  10,10,0,
+  0,10,0,
+  0,10,10,
+  0,0,10,
+  10,0,10,
+  10,5,0,
+  5,16,5};
 
 void loop(void)
 {
@@ -208,9 +207,9 @@ void loop(void)
     default:
       network.read(header,0,0); // if none of the above packet types matched, read out and flush the buffer
       if (DEBUG) {
-      Serial.print(F("            undefined packet type: ")); // print the unrecognized packet type
-      Serial.print(header.type);
-      Serial.println();
+        Serial.print(F("            undefined packet type: ")); // print the unrecognized packet type
+        Serial.print(header.type);
+        Serial.println();
       }
       break;
     };
@@ -223,7 +222,7 @@ void loop(void)
   {
     ledst(2);
     /* // unsigned long int rollover checking:
-    Serial.print(microsRollover()); // how many times has the unsigned long micros() wrapped?
+     Serial.print(microsRollover()); // how many times has the unsigned long micros() wrapped?
      Serial.print(":"); //separator 
      Serial.print(nowM); //micros();
      Serial.print("\n"); //new line
@@ -234,51 +233,51 @@ void loop(void)
     updates = 0;
     last_time_sent = now;
     if (KEEPALIVE) {
-    uint16_t to = 000;
-    bool ok = 0;
-    if ( to != this_node)
-    {
-      unsigned long nowM = micros();
-      ok = send_T(to);
-      if (DEBUG) {
-      p(" in %ld us.\n", (micros()-nowM) );
-      }
-      if (ok){
-        p_sent++;
-      }
-      if (!ok)
+      uint16_t to = 000;
+      bool ok = 0;
+      if ( to != this_node)
       {
-        errors++;
+        unsigned long nowM = micros();
+        ok = send_T(to);
         if (DEBUG) {
-        p("%010ld: No ACK timeout.\n", millis()); // An error occured, need to stahp!
+          p(" in %ld us.\n", (micros()-nowM) );
         }
-      }
-      
-      iterations++;
-      /*
+        if (ok){
+          p_sent++;
+        }
+        if (!ok)
+        {
+          errors++;
+          if (DEBUG) {
+            p("%010ld: No ACK timeout.\n", millis()); // An error occured, need to stahp!
+          }
+        }
+
+        iterations++;
+        /*
       Serial.print("loop: \t\t");
-       Serial.println(iterations);
-       Serial.print("errors: \t\t");
-       Serial.println(errors);     
-       Serial.print("send error in %: \t");
-       Serial.println(errors*100/iterations);
-       Serial.print("pkts sent    : \t");
-       Serial.println(p_sent);
-       Serial.print("pkts received: \t");
-       Serial.println(p_recv);
-       Serial.print("replies in %: \t");
-       Serial.println(p_recv*100/(p_sent-1));
-       */
-       }
+         Serial.println(iterations);
+         Serial.print("errors: \t\t");
+         Serial.println(errors);     
+         Serial.print("send error in %: \t");
+         Serial.println(errors*100/iterations);
+         Serial.print("pkts sent    : \t");
+         Serial.println(p_sent);
+         Serial.print("pkts received: \t");
+         Serial.println(p_recv);
+         Serial.print("replies in %: \t");
+         Serial.println(p_recv*100/(p_sent-1));
+         */
+      }
     }
     sweep+=20;
     if (sweep>254) sweep=0;
-    
+
     if ( this_node == 00){
-    for (short _i=0; _i<num_active_nodes; _i++) {
-    send_L1(active_nodes[_i],(byte) sweep&0xFF);
-    }
-    strobe=!strobe;
+      for (short _i=0; _i<num_active_nodes; _i++) {
+        send_L1(active_nodes[_i],(byte) sweep&0xFF);
+      }
+      strobe=!strobe;
     }
   }
 }
@@ -296,43 +295,43 @@ void loop(void)
  */
 void send_L1(int to, int _b = 0){
   if ( to != this_node) {      
-     /* byte ledmap[24]={
-        1,2,3,
-        15,(((byte) millis()&0xFF)/10),(((byte) millis()+127&0xFF)/10),
-        5,15,5,
-        0,5,15,
-        15,5,0,
-        5,15,5,
-        0,5,15,
-        5,0,(((byte) millis()&0xFF)/10)};
-        
-        ledmap[min(23,to*3)]=((byte) millis()&0xFF);
-     */
+    /* byte ledmap[24]={
+     1,2,3,
+     15,(((byte) millis()&0xFF)/10),(((byte) millis()+127&0xFF)/10),
+     5,15,5,
+     0,5,15,
+     15,5,0,
+     5,15,5,
+     0,5,15,
+     5,0,(((byte) millis()&0xFF)/10)};
      
-     byte ledmap[24]={
-        1,2,3,
-        0,0,_b,
-        0,0,_b,
-        0,0,_b,
-        0,0,_b,
-        0,0,_b,
-        0,0,_b,
-        0,(((byte) millis()&0xFF)/5),_b};
-        
-        //ledmap[min(23,to+3)]=((byte) sweep&0xFF);
-        //ledmap[min(23,to+4)]=(((byte) sweep+127)&0xFF);
-        //ledmap[min(23,to+5)]=(((byte) sweep)&0xFF);
-        
-/*      Serial.println();
-        for(uint16_t i=0; i<sizeof(ledmap); i++) { // print out the packet via serial
-        Serial.print(ledmap[i]);
-        Serial.print(" ");
-      }
-      Serial.println();
-*/
-      unsigned long now = millis();
-      bool ok = send_L(to, ledmap);
-      if (DEBUG) {
+     ledmap[min(23,to*3)]=((byte) millis()&0xFF);
+     */
+
+    byte ledmap[24]={
+      1,2,3,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,(((byte) millis()&0xFF)/5),_b    };
+
+    //ledmap[min(23,to+3)]=((byte) sweep&0xFF);
+    //ledmap[min(23,to+4)]=(((byte) sweep+127)&0xFF);
+    //ledmap[min(23,to+5)]=(((byte) sweep)&0xFF);
+
+    /*      Serial.println();
+     for(uint16_t i=0; i<sizeof(ledmap); i++) { // print out the packet via serial
+     Serial.print(ledmap[i]);
+     Serial.print(" ");
+     }
+     Serial.println();
+     */
+    unsigned long now = millis();
+    bool ok = send_L(to, ledmap);
+    if (DEBUG) {
       p(" in %ld ms.\n", (millis()-now) );
       if (ok){
       }
@@ -340,15 +339,15 @@ void send_L1(int to, int _b = 0){
       {
         p("%010ld: send_L timout.\n", millis()); // An error occured..
       }
-      }
-      ledst();
     }
+    ledst();
   }
-    
+}
+
 boolean send_T(uint16_t to) // Send out this nodes' time -> Timesync!
 {
   if (DEBUG) {
-  p("%010ld: Sent 'T' to   %05o", millis(),to);
+    p("%010ld: Sent 'T' to   %05o", millis(),to);
   }
   RF24NetworkHeader header(to,'T');
   unsigned long time = micros();
@@ -358,7 +357,7 @@ boolean send_T(uint16_t to) // Send out this nodes' time -> Timesync!
 boolean send_L(uint16_t to, byte* ledmap) // Send out an LED map
 {
   if (DEBUG) {
-  p("%010ld: Sent 'L' to   %05o", millis(),to);
+    p("%010ld: Sent 'L' to   %05o", millis(),to);
   }
   RF24NetworkHeader header(to,'L');
   return network.write(header,ledmap,24);
@@ -377,15 +376,15 @@ void handle_K(RF24NetworkHeader& header)
   byte kmap[24];
   network.read(header,kmap,sizeof(kmap));
   if (DEBUG) {
-  p("%010ld: Recv 'K' from %05o\n", millis(), header.from_node);
+    p("%010ld: Recv 'K' from %05o\n", millis(), header.from_node);
   }
   ledupdate(kmap);
-if (DEBUG) {
-  for(uint16_t i=0; i<sizeof(kmap); i++) { // print out the received packet via serial
-    Serial.print(kmap[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+  if (DEBUG) {
+    for(uint16_t i=0; i<sizeof(kmap); i++) { // print out the received packet via serial
+      Serial.print(kmap[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
 }
 
@@ -394,15 +393,15 @@ void handle_L(RF24NetworkHeader& header)
   byte ledmap[24];
   network.read(header,ledmap,sizeof(ledmap));
   if (DEBUG) {
-  p("%010ld: Recv 'L' from %05o\n", millis(), header.from_node);
+    p("%010ld: Recv 'L' from %05o\n", millis(), header.from_node);
   }
   ledupdate(ledmap);
   if (DEBUG) {
-  for(uint16_t i=0; i<sizeof(ledmap); i++) { // print out the received packet via serial
-    Serial.print(ledmap[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+    for(uint16_t i=0; i<sizeof(ledmap); i++) { // print out the received packet via serial
+      Serial.print(ledmap[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
 }
 
@@ -411,7 +410,7 @@ void handle_T(RF24NetworkHeader& header)
   unsigned long time;
   network.read(header,&time,sizeof(time));
   if (DEBUG) {
-  p("%010ld: Recv 'T' from %05o:%010ld\n", millis(), header.from_node, time);
+    p("%010ld: Recv 'T' from %05o:%010ld\n", millis(), header.from_node, time);
   }
   add_node(header.from_node);  
   if(header.from_node != this_node)
@@ -419,11 +418,11 @@ void handle_T(RF24NetworkHeader& header)
     RF24NetworkHeader header2(header.from_node/*header.from_node*/,'B');
     unsigned long nowM = micros();
     if(network.write(header2,&time,sizeof(time)))
-    if (DEBUG) {
-      p("%010ld: Answ 'B' to   %05o in ", millis(),header.from_node);
-      Serial.print(micros()-nowM-16);
-      Serial.print(F(" us.\n"));
-    }
+      if (DEBUG) {
+        p("%010ld: Answ 'B' to   %05o in ", millis(),header.from_node);
+        Serial.print(micros()-nowM-16);
+        Serial.print(F(" us.\n"));
+      }
   }
 }
 
@@ -433,7 +432,7 @@ void handle_B(RF24NetworkHeader& header)
   unsigned long ref_time;
   network.read(header,&ref_time,sizeof(ref_time));
   if (DEBUG) {
-  p("%010ld: Recv 'B' from %05o -> %ldus round trip\n", millis(), header.from_node, micros()-ref_time);
+    p("%010ld: Recv 'B' from %05o -> %ldus round trip\n", millis(), header.from_node, micros()-ref_time);
   }
 }
 
@@ -456,7 +455,7 @@ void add_node(uint16_t node) //TODO: remove_node, after a certain timeout...
   {
     active_nodes[num_active_nodes++] = node; 
     if (DEBUG) {
-    p("%010ld: New node: %05o\n", millis(), node);
+      p("%010ld: New node: %05o\n", millis(), node);
     }
   }
 }
@@ -491,4 +490,5 @@ void colorWipe(uint32_t c, uint8_t wait) { //this is blocking with the hardcoded
     delay(wait);
   }
 }
+
 
